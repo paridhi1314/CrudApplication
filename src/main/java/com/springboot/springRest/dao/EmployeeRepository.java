ad7950cd60dc5c4e71ce5a3b8d0de7e7af4a7c24
Paridhi Malav <paridhi.malav@incture.com>
2024-08-12 19:10:06 +0530

    Spring Data Rest

--- src/main/java/com/springboot/springRest/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/springRest/dao/EmployeeRepository.java
@@ -1,11 +1,12 @@
 package com.springboot.springRest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.springRest.entities.Employee;
 
-
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
